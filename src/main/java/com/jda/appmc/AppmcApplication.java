18d362a03ec70ef30057fb7a074be7ac563d60c7
jaderdeandrade <jaderdeandrade@gmail.com>
2019-06-29 10:14:26 -0300

    Projeto criado

---
+++ src/main/java/com/jda/appmc/AppmcApplication.java
@@ -0,0 +1,13 @@
+package com.jda.appmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmcApplication.class, args);
+	}
+
+}
