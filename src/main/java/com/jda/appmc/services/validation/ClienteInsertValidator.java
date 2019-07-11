fe86ac61fd992ad74de76a4143fc1337a4b0f12e
jaderdeandrade <jaderdeandrade@gmail.com>
2019-07-11 15:06:25 -0300

    Validação customizada de CPF ou CPNJ na inserção do cliente

---
+++ src/main/java/com/jda/appmc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,28 @@
+package com.jda.appmc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.jda.appmc.dto.ClienteNewDTO;
+import com.jda.appmc.resouces.exception.FieldMessage;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+// inclua os testes aqui, inserindo erros na lista
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
