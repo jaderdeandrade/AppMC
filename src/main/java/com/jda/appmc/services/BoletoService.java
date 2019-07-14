27439cb8365e1c2ce6948eb51d185b1b7c976410
jaderdeandrade <jaderdeandrade@gmail.com>
2019-07-14 10:16:26 -0300

    Inserindo Pedidos

---
+++ src/main/java/com/jda/appmc/services/BoletoService.java
@@ -0,0 +1,19 @@
+package com.jda.appmc.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.jda.appmc.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instanteDoPedido);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pagto.setDataVencimento(cal.getTime());
+	}
+}
--- src/main/java/com/jda/appmc/services/PedidoService.java
+++ src/main/java/com/jda/appmc/services/PedidoService.java
@@ -1,25 +1,87 @@
 package com.jda.appmc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.jda.appmc.domain.Cliente;
+import com.jda.appmc.domain.ItemPedido;
+import com.jda.appmc.domain.PagamentoComBoleto;
 import com.jda.appmc.domain.Pedido;
+import com.jda.appmc.domain.enums.EstadoPagamento;
+import com.jda.appmc.repositories.ItemPedidoRepository;
+import com.jda.appmc.repositories.PagamentoRepository;
 import com.jda.appmc.repositories.PedidoRepository;
 import com.jda.appmc.services.exceptions.ObjectNotFoundException;
 
+
 @Service
 public class PedidoService {
-
+	
 	@Autowired
 	private PedidoRepository repo;
-
-	public Pedido buscar(Integer id) {
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ClienteService clienteService;
+	/*
+	@Autowired
+	private EmailService emailService;*/
+	
+	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
-
 	}
-
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		//emailService.sendOrderConfirmationEmail(obj);
+		return obj;
+	}
+	
+/*	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}*/
 }
