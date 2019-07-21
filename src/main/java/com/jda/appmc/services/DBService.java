5a7d27a12bce4c12bced773cc1e8395d9fa407c5
jaderdeandrade <jaderdeandrade@gmail.com>
2019-07-21 17:06:16 -0300

    Implementando autenticacao e geracao do token JWT

--- src/main/java/com/jda/appmc/services/DBService.java
+++ src/main/java/com/jda/appmc/services/DBService.java
@@ -114,7 +114,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "nelio.cursos@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		Cliente cli1 = new Cliente(null, "Jader de Andrade", "jaderdeandrade@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
@@ -124,7 +124,7 @@ public class DBService {
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
-		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "281777012", cli2, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro3t", "281777012", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		cli2.getEnderecos().addAll(Arrays.asList(e3));
